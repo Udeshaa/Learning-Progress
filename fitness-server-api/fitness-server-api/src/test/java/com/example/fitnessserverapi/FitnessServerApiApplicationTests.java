1f4e6320538394fa57381e909cc53fb23d12da72
Udeshaa <udesha77722@gmail.com>
2025-05-07 18:07:12 +0530

    Update FitnessServerApiApplicationTests.java

--- fitness-server-api/fitness-server-api/src/test/java/com/example/fitnessserverapi/FitnessServerApiApplicationTests.java
+++ fitness-server-api/fitness-server-api/src/test/java/com/example/fitnessserverapi/FitnessServerApiApplicationTests.java
@@ -1,7 +1,7 @@
 package com.example.fitnessserverapi;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest; 
 
 @SpringBootTest
 class FitnessServerApiApplicationTests {
